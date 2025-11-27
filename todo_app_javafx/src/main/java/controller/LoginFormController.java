c647704b426b0f8246ea23890dfa356806207e86
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-27 11:49:32 +0530

    Design Login Page

---
+++ todo_app_javafx/src/main/java/controller/LoginFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginFormController {
+
+    @FXML
+    private JFXTextField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    void btnLogInOnAction(ActionEvent event) {
+
+    }
+
+}
