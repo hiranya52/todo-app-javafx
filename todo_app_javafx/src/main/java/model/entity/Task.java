2b0f0f21ff2897f73319f4ba78e9e2fae34e7f39
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-27 15:37:00 +0530

    Add Task Entity

--- todo_app_javafx/src/main/java/model/entity/Task.java
+++ todo_app_javafx/src/main/java/model/entity/Task.java
@@ -4,9 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.*;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
 public class Task {
 
-
+    @Id
+    private int id;
+    private String date;
+    private String title;
+    private String Description;
 
 }
