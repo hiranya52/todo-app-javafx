2477b2229cb1a7f8cde7ff95508d6bf6c09e5355
Hiranya Mendis <hiranyamendis52@gmail.com>
2025-11-27 14:49:49 +0530

    Add login button functionality

--- todo_app_javafx/src/main/java/controller/LoginFormController.java
+++ todo_app_javafx/src/main/java/controller/LoginFormController.java
@@ -1,19 +1,43 @@
 package controller;
 
+import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
 
     @FXML
-    private JFXTextField txtPassword;
+    private JFXPasswordField txtPassword;
 
     @FXML
     private JFXTextField txtUsername;
 
     @FXML
     void btnLogInOnAction(ActionEvent event) {
+
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+
+        if (username.equals("sehi") && password.equals("1234")){
+            try {
+                // Load dashboard
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/dashboard.fxml"));
+                Scene scene = new Scene(loader.load());
+
+                // Get current stage from event source
+                Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+
+                stage.setScene(scene);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
     }
 
